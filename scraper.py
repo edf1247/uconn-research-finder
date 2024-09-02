b6dec56b98bfd4e36f98159866bb5d5de1187cb8
edf1247 <edf1247@gmail.com>
2024-09-02 00:02:22 -0400

    used regex to select the url in each a tag, store them in an array and remove duplicates

--- scraper.py
+++ scraper.py
@@ -1,7 +1,30 @@
 import bs4
+import re
 
 f = open("source.txt", "r", encoding="utf-8")
 soup = bs4.BeautifulSoup(f, "html.parser")
 f.close()
 
 
+links = soup.find_all("a", attrs={"class": "person-permalink"})
+
+for link in links:
+    for l in link.findChildren():
+        l.extract()
+
+arr = []
+
+for link in links:
+    arr.append(str(link))
+
+link_arr = []
+pattern = r'href="([^"]*)"'
+
+for link in arr:
+    match = re.search(pattern, link)
+    if(match):
+        link_arr.append(match.group(1))
+
+clean_links = [i for n, i in enumerate(link_arr) if i not in link_arr[:n]]
+
+print(len(clean_links))
